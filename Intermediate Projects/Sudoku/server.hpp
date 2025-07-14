c943355434d4cea41411de312bed0c7c734f2d42
Bidhu Bhusan Patra <bidhu.patra@outlook.com>
2025-07-14 19:26:33 +0530

    Delete Intermediate Projects/Sudoku directory

--- Intermediate Projects/Sudoku/server.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SERVER_HPP
-#define SERVER_HPP
-
-// Declare functions from server.cpp to expose for testing
-void handle_client(int client_fd);
-int start_server();
-
-#endif  // SERVER_HPP
